38b44dee52ad4c05df5d205f9da85fff7ff07807
ergo <602507143@qq.com>
2018-05-30 22:25:12 +0800

    half an hour for slider but the result is --just learn vue

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -1,23 +1,33 @@
-(function(window){
+(function(window) {
   'use strict';
-  var APP = window.APP||{};
-  var $=window.jQuery;
-  function FormHandler(selector){
-    if(!selector){
+  var APP = window.APP || {};
+  var $ = window.jQuery;
+
+  function FormHandler(selector) {
+    if (!selector) {
       throw new Error('No selector provided');
     }
     this.$formElement = $(selector);
     if (this.$formElement.length === 0) {
       throw new Error('Could not find element with selector')
     }
+    var strengthLevelNum = $(".strengthLevelNum");
+    // $(".selector").slider({
+    //   change: function(event, ui) {
+    //
+    //   }
+    // });
+    $( "#strengthLevel" ).on( "change", function( event) {
+      strengthLevelNum.text(this.value);
+    } );
   }
-  FormHandler.prototype.addSubmitHandler = function(fn){
-    this.$formElement.on('submit',function(event){
+  FormHandler.prototype.addSubmitHandler = function(fn) {
+    this.$formElement.on('submit', function(event) {
       event.preventDefault();
 
       var data = {};
-      $(this).serializeArray().forEach(function(item){
-        data[item.name]=item.value;
+      $(this).serializeArray().forEach(function(item) {
+        data[item.name] = item.value;
       });
       console.log(data);
       fn(data);
