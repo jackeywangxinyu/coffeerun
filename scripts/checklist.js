b0f040c786f78b2622047842fa227c419d7c921c
ergo <602507143@qq.com>
2018-05-31 22:14:01 +0800

    new checklist

---
+++ scripts/checklist.js
@@ -0,0 +1,71 @@
+(function(window) {
+  var APP = window.APP||{};
+  var $ = window.jQuery;
+  var forColor = {
+    '':'blue',
+    'caramel':'green',
+    'almond':'yellow',
+    'mocha':'red'
+  }
+  // console.log(forColor['']);
+  function CheckList(selector){
+    if(!selector){
+      throw new Error("No selector provided");
+    }
+    this.$element = $(selector);
+    if(this.$element.length===0){
+      throw new Error("Could not find element with selector:"+seletor);
+    }
+
+  }
+
+  function Row(coffeeOrder){
+    var $div = $('<div></div>',{
+      'data-coffee-order':'checkbox',
+      'class':'checkbox'
+    });
+    $div.css('background-color',forColor[coffeeOrder.flavor]);
+    var $label = $('<label></label>');
+    var $checkbox = $('<input></input>',{
+      'type':'checkbox',
+      'value':coffeeOrder.emailAddress
+    });
+    var description = coffeeOrder.size+' ';
+    if (coffeeOrder.flavor) {
+      description+=coffeeOrder.flavor+' ';
+    }
+    description+=coffeeOrder.coffee+' ';
+    description+=' ('+coffeeOrder.emailAddress+') ';
+    description+=' ['+coffeeOrder.strength+'] ';
+
+    $label.append($checkbox);
+    $label.append(description);
+    $div.append($label);
+
+    this.$element = $div;
+  }
+  CheckList.prototype.addRow = function(coffeeOrder){
+
+    this.removeRow(coffeeOrder.emailAddress);
+    var rowElement = new Row(coffeeOrder);
+
+    this.$element.append(rowElement.$element);
+  }
+  CheckList.prototype.removeRow = function(email){
+    this.$element
+    .find('[value="'+email+'"]')
+    .closest('[data-coffee-order="checkbox"]')
+    .remove();
+  }
+
+  CheckList.prototype.addClickHandler = function(fn){
+    this.$element.on('click','input',function(event){
+      var email = event.target.value;
+      this.removeRow(email);
+      fn(email);
+    }.bind(this));
+  }
+
+  APP.CheckList = CheckList;
+  window.APP = APP;
+})(window)
--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,22 @@
 (function(window){
   'use strict';
   var FROM_SELECTOR = '[data-coffee-order="form"]';
+  var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
   var APP = window.APP;
   var Truck = APP.Truck;
   var DataStore = APP.DataStore;
   var FormHandler = APP.FormHandler;
+  var CheckList = APP.CheckList;
 
   var myTruck = new Truck('ncc-1701',new DataStore());
+  var checklist = new CheckList(CHECKLIST_SELECTOR);
   window.myTruck = myTruck;
   var formhandler = new FormHandler(FROM_SELECTOR);
-  formhandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+  formhandler.addSubmitHandler(function(data){
+    myTruck.createOrder.call(myTruck,data);
+    checklist.addRow.call(checklist,data);
+  });
+
+  checklist.addClickHandler(myTruck.deliverOrder.bind(myTruck));
 
 })(window);
